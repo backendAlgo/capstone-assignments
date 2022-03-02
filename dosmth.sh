55517761ac882ccc39ab1a39cf67ba67f4cafee7
backendAlgo <mirshod.mirjonov99@gmail.com>
2022-03-02 16:25:25 +0500

    asdjasldj

---
+++ dosmth.sh
@@ -0,0 +1,5 @@
+# !usr/bin/env bash
+rm -rf capstone-assignments
+git clone https://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@github.com/backendAlgo/capstone-assignments.git
+cd capstone-assignments
+mkdir build
