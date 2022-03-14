f8b2958ff6c063162023dd18ae2135eaf9769c62
Begzod Muxamedov <bmukhamedov585@bk.ru>
2022-03-14 21:10:40 +0500

    fix: typo

--- midterm/10-line_tracer_B.c
+++ midterm/10-line_tracer_B.c
@@ -49,7 +49,7 @@ bool test1 = false;
 
 int const MAX_SPEED = 100;
 int NORM_SPEED = MAX_SPEED/4;
-int MID_SPEED = MID_SPEED/8;
+int MID_SPEED = MAX_SPEED/8;
 int const MIN_SPEED = 0;
 
 int main(void) {
