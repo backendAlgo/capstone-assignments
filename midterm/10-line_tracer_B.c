c3b736254c35a3ea3dd8746e4df011fa1d8fe347
Begzod Muxamedov <bmukhamedov585@bk.ru>
2022-03-15 14:31:43 +0500

    speed

--- midterm/10-line_tracer_B.c
+++ midterm/10-line_tracer_B.c
@@ -54,8 +54,8 @@ int rightTracer;
 bool test1 = false;
 
 int const MAX_SPEED = 100;
-int NORM_SPEED = 50;
-int MID_SPEED = 15;
+int NORM_SPEED = 60;
+int MID_SPEED = 25;
 int const MIN_SPEED = 0;
 
 // int turnDelay = 200;
