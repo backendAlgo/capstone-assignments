6b0aadd80471bf2d83a43dc3603e534901bb1d16
Begzod Muxamedov <bmukhamedov585@bk.ru>
2022-03-15 16:01:15 +0500

    check dist

--- midterm/sensors.c
+++ midterm/sensors.c
@@ -11,8 +11,13 @@
 #define RIGHT_IR_PIN 26
 #define RIGHT_TRACER_PIN 11
 
+#define TRIG_PIN		28
+#define ECHO_PIN		29
+int dist;
+
 void initIR();
 void initLineTacer();
+void initUltrasonic();
 
 int leftIR;
 int mLeftIR;
@@ -26,9 +31,12 @@ int main(void){
 	    return 0;
   
     initIR();
-
+    initUltrasonic();
 
     while (1) {
+        dist = getDistance();
+		printf("Distance: %d\n", dist);
+
         leftIR = !digitalRead(LEFT_TRACER_PIN); // 1 will be white
         mLeftIR = !digitalRead(LEFT_IR_PIN);
         mRightIR = !digitalRead(RIGHT_IR_PIN);
@@ -94,5 +102,27 @@ void initLineTacer() {
     pinMode(LEFT_TRACER_PIN, INPUT);
     pinMode(RIGHT_TRACER_PIN, INPUT);
 }
+int getDistance()
+{
+	int 		start_time=0, end_time=0;
+	float 	distance=0;
+
+    digitalWrite(TRIG_PIN, LOW) ;
+    delay(30) ;
+    digitalWrite(TRIG_PIN, HIGH) ;
+    delayMicroseconds(10) ;
+    digitalWrite(TRIG_PIN, LOW) ;
+    
+    while (digitalRead(ECHO_PIN) == 0) ;
+    start_time = micros() ;
+    
+    while (digitalRead(ECHO_PIN) == 1) ;
+    end_time = micros() ;
+    
+    distance = (end_time - start_time) / 29. / 2. ;
+	
+	return (int)distance;
+}
+
 //gcc 6-ir_sensor_ex.c -o irex -lwiringPi
 // ./irex
