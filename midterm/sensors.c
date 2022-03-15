257661b1e0eaedf5a24ba96db39be5e3b5635bdd
Begzod Muxamedov <bmukhamedov585@bk.ru>
2022-03-15 13:12:53 +0500

    feat: array

--- midterm/sensors.c
+++ midterm/sensors.c
@@ -18,6 +18,7 @@ int leftIR;
 int mLeftIR;
 int mRightIR; 
 int rightIR;
+int sensors[4];
 
 int main(void){
     
@@ -32,7 +33,23 @@ int main(void){
         mLeftIR = !digitalRead(LEFT_IR_PIN);
         mRightIR = !digitalRead(RIGHT_IR_PIN);
         rightIR = !digitalRead(RIGHT_TRACER_PIN);
+        sensors = {leftIR, mLeftIR, mRightIR, rightIR};
         printf("%d %d %d %d\n", leftIR, mLeftIR, mRightIR, rightIR);
+        printf("%d %d %d %d\n", sensors[0], sensors[1], sensors[2], sensors[3]);
+        // Forward
+        if (!leftIR && !mLeftIR && !mRightIR && !rightIR) {
+            printf("Go Forward")
+        }
+        else if (!leftIR && mLeftIR && mRightIR && !rightIR) {
+            printf("Go Forward 2")
+        }
+        // Stop
+        else if (leftIR && !mLeftIR && !mRightIR && rightIR) {
+            printf("Stop")
+        }
+        else if (leftIR && mLeftIR && mRightIR && rightIR) {
+            printf("Stop 2")
+        }
 
         // if(mLeftIR == 1 && mRightIR == 0 ) {
         //     printf("Right\n");
