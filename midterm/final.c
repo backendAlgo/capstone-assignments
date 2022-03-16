8e7bdf3f901ece4d2ecb93bc2b1e904ad47d643f
Begzod Muxamedov <bmukhamedov585@bk.ru>
2022-03-16 08:20:12 +0500

    go

--- midterm/final.c
+++ midterm/final.c
@@ -126,7 +126,7 @@ int main(void) {
 			}
         }
 		else {
-			if (count == 2 && !leftOBS || obsCounterL > 0) {
+			if (count == 2 && !leftOBS && obsCounterL > 0) {
 				while (!leftOBS) {
 					leftOBS = !digitalRead(LEFT_OBS);
 					smoothLeft(2);
